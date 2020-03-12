4aaa7ba3e7d36e1093c4009736b8d81ef395cff8
xtixi <55737311+xtixi@users.noreply.github.com>
2020-03-12 21:03:08 +0300

    Add files via upload

---
+++ TimerTop/top/top/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace top
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dur = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-7, -18);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(365, 251);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(62, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(379, 516);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(38, 33);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "vvv";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(379, 435);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "^^^";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(336, 475);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "<<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 475);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = ">>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dur
+            // 
+            this.dur.Location = new System.Drawing.Point(380, 479);
+            this.dur.Name = "dur";
+            this.dur.Size = new System.Drawing.Size(30, 25);
+            this.dur.TabIndex = 11;
+            this.dur.Text = "dur";
+            this.dur.UseVisualStyleBackColor = true;
+            this.dur.Click += new System.EventHandler(this.dur_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dur);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button dur;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TimerTop/top/top/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace top
+{
+    public partial class Form1 : Form
+    {
+
+        int a=0;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 200;
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            a = 1;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            a = 3;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            a = 2;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            a = 4;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if(a==1)
+            { 
+                pictureBox1.Left += 20;
+            }
+            else if(a==2)
+            {
+                pictureBox1.Left += -20;
+            }
+            else if (a == 3)
+            {
+                pictureBox1.Top += -20;
+            }
+            else if (a == 4)
+            {
+                pictureBox1.Top += 20;
+            }
+        }
+
+        private void dur_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
